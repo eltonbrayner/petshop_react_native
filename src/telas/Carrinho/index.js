234f08ed87e3a633838be42ab9aa7c6c028d6a17
Elton Brayner <eltonbrayner@iMac-de-Elton.local>
2021-09-16 14:44:23 -0300

    fix scroll on android device

--- src/telas/Carrinho/index.js
+++ src/telas/Carrinho/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 import {
   SafeAreaView,
@@ -6,48 +6,53 @@ import {
   FlatList,
   KeyboardAvoidingView,
   Platform,
-} from "react-native";
+  ScrollView,
+} from 'react-native';
 
-import Item from "./Item";
-import TelaPadrao from "../../componentes/TelaPadrao";
-import StatusCarrinho from "../../componentes/StatusCarrinho";
+import Item from './Item';
+import TelaPadrao from '../../componentes/TelaPadrao';
+import StatusCarrinho from '../../componentes/StatusCarrinho';
 
 const servicos = [
   {
     id: 1,
-    nome: "Banho",
+    nome: 'Banho',
     preco: 79.9,
-    descricao: "NÃO DE BANHO NO SEU GATO! Mas se precisar nós damos.",
-    quantidade:1
+    descricao: 'NÃO DE BANHO NO SEU GATO! Mas se precisar nós damos.',
+    quantidade: 1,
   },
   {
     id: 2,
-    nome: "Vacina V4",
+    nome: 'Vacina V4',
     preco: 89.9,
-    descricao: "Uma dose da vacina V4. Seu gato precisa de duas.",
-    quantidade:1
+    descricao: 'Uma dose da vacina V4. Seu gato precisa de duas.',
+    quantidade: 1,
   },
   {
     id: 3,
-    nome: "Vacina Antirrábica",
+    nome: 'Vacina Antirrábica',
     preco: 99.9,
     descricao:
-      "Uma dose da vacina antirrábica. Seu gato precisa de uma por ano.",
-      quantidade:1
+      'Uma dose da vacina antirrábica. Seu gato precisa de uma por ano.',
+    quantidade: 1,
   },
 ];
 
 export default function Carrinho() {
-    const total = servicos.reduce((soma, {preco, quantidade}) => soma + (preco * quantidade), 0)
+  const total = servicos.reduce(
+    (soma, { preco, quantidade }) => soma + preco * quantidade,
+    0,
+  );
   return (
-    <TelaPadrao> 
-        <StatusCarrinho total={total}/>
+    <ScrollView>
+      <TelaPadrao>
+        <StatusCarrinho total={total} />
         <FlatList
           data={servicos}
           renderItem={({ item }) => <Item {...item} />}
           keyExtractor={({ id }) => String(id)}
         />
-    </TelaPadrao>    
-      
+      </TelaPadrao>
+    </ScrollView>
   );
 }
